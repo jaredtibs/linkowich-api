3335cb8a365ff923865fd20a5a0bdb86b23823a0
Jared Tibshraeny <jmtibs@gmail.com>
2017-07-16 18:43:19 -0700

    user follow unfollow convenience methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,15 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
   has_many :following, through: :following_relationships, source: :following
 
+  def follow(user_id)
+    following_relationships.create following_id: user_id
+  end
+
+  def unfollow(user_id)
+    following_relationship = following_relationships.find_by following_id: user_id
+    following_relationship ? following_relationship.destroy : false
+  end
+
   def image_data=(data)
     # decode data and create stream on them
     io = CarrierStringIO.new(Base64.decode64(data))

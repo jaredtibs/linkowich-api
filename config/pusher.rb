a75803977a1c2f85b2efc8473e97a2834dd0987e
Jared Tibshraeny <jmtibs@gmail.com>
2017-11-03 00:11:41 -0700

    update pusher config

--- config/pusher.rb
+++ config/pusher.rb
@@ -3,5 +3,6 @@ require 'pusher'
 Pusher.app_id = ENV['PUSHER_APP_ID']
 Pusher.secret = ENV['PUSHER_APP_SECRET']
 Pusher.key = ENV['PUSHER_API_KEY']
+Pusher.cluser = 'us2'
 Pusher.logger = Rails.logger
 Pusher.encrypted = true

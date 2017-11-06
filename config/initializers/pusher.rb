790a22b4a7c46d3d4b431760df3483a2d446242b
Jared Tibshraeny <jmtibs@gmail.com>
2017-11-05 20:00:57 -0800

    finish be config

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,19 +1,8 @@
 require 'pusher'
 
-$pusher_client = Pusher::Client.new(
-  app_id: ENV['PUSHER_APP_ID'],
-  key: ENV['PUSHER_API_KEY'],
-  secret: ENV['PUSHER_APP_SECRET'],
-  cluster: 'us2',
-  logger: Rails.logger,
-  encrypted: true
-)
-
-=begin
 Pusher.app_id = ENV['PUSHER_APP_ID']
 Pusher.secret = ENV['PUSHER_APP_SECRET']
 Pusher.key = ENV['PUSHER_API_KEY']
 Pusher.cluster = 'us2'
 Pusher.logger = Rails.logger
 Pusher.encrypted = true
-=end

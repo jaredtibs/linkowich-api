5f14e837eeae965597f0ca8ad890eccb24b080bd
Jared Tibshraeny <jmtibs@gmail.com>
2018-02-03 22:53:05 -0800

    set up new email files and trigger points

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::InvitationsController < Api::V1::BaseController
   def create
     @invitation = current_user.invitations.build recipient_email: params[:email]
     if @invitation.save
-      #UserMailer.app_invite(current_user, params[:email]).deliver_later
+      UserMailer.invitation_email(current_user, params[:email]).deliver_later
       render json: { success: "Invite sent" }, status: :ok
     else
       render json: { errors: "Unable to send invite" }, status: :unprocessable_entity

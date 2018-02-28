4712adfca1220da8d27672f5dfb0df6198dd84a8
Jared Tibshraeny <jmtibs@gmail.com>
2018-02-27 22:03:54 -0800

    syntax error urls

--- app/models/link.rb
+++ app/models/link.rb
@@ -22,7 +22,7 @@ class Link < ApplicationRecord
   private
 
   def smart_add_url_protocol
-    unless self.url[/^https?:\/\//] || self.url[/^http?:\/\//]]
+    unless self.url[/^https?:\/\//] || self.url[/^http?:\/\//]
       self.url = "http://#{self.url}"
     end
   end
